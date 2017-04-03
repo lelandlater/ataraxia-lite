c958d432477fe50a1b3386059a209a8b97e96fb4
Leland Later <leland@ip-172-31-0-42.us-west-1.compute.internal>
2017-04-03 19:08:13 +0000

    Server changes to nginx using vi on EC2.

--- web/run.py
+++ web/run.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 from htmlmin.minify import html_minify
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = 'ataraxialocal'
+app.config['SERVER_NAME'] = 'ataraxia.io'
 
 """ATARAXIA in Greek."""
 ataraxia = '&#x3AC;&#x3C4;&#x3B1;&#x3C1;&#x3B1;&#x3BE;&#x3AF;&#x3B1;'
