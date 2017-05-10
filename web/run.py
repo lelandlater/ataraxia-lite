11529f7d81c66abd652d0b6a5f0823812198f244
Leland Later <lelandlater@gmail.com>
2017-05-10 12:06:48 -0700

    Radically redoing the CSS in plzace of JS for responsive design.

--- web/run.py
+++ web/run.py
@@ -2,31 +2,46 @@ from flask import Flask, render_template, url_for
 from htmlmin.minify import html_minify
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = 'ataraxia.io'
+app.config['SERVER_NAME'] = 'ataraxialocal'
+
 
 """ATARAXIA in Greek."""
 ataraxia = '&#x3AC;&#x3C4;&#x3B1;&#x3C1;&#x3B1;&#x3BE;&#x3AF;&#x3B1;'
 
-"""For navigation bar."""
+
+
+"""
+Generalized navigation. NavBar passed to Jinja, composed of two NavItem lists.
+"""
+
 class NavItem(object):
 
-    def __init__(self, url, label, homelogo=False):
-        self.func_url = url
+    def __init__(self, url, label):
+        self.url = url
         self.label = label
-        self.homelogo = homelogo
 
+class NavBar(object):
+
+	"""
+	Two lists: starting and ending NavItems for use with flexbox layout.
+	"""
+	def __init__(self, start, end)
+		self.start = start
+		self.end = end
+
+"""Index page displaying logo and link to contact page."""
 @app.route('/')
 @app.route('/index')
 def index():
-	nav = [NavItem(url_for('index'), ataraxia.encode('unicode-escape'), True),
-           NavItem(url_for('contact'), 'CONTACT')]
-	rendered = render_template('index.html', navitems=nav)
+	nav = NavBar([NavItem(url_for('index'), ataraxia.encode('unicode-escape')], [NavItem(url_for('contact'), 'CONTACT')])]
+	rendered = render_template('index.html', nav=nav)
 	return html_minify(rendered)
 
+"""Display contact links and link back to index page."""
 @app.route('/contact')
 def contact():
-	nav = [NavItem(url_for('index'), ataraxia.encode('unicode-escape'), True)]
-	rendered = render_template('contact.html', navitems=nav)
+	nav = NavBar([NavItem(url_for('index'), ataraxia.encode('unicode-escape')], [NavItem(url_for('contact'), 'CONTACT')])
+	rendered = render_template('contact.html', nav=nav)
 	return html_minify(rendered)
 
 if __name__=='__main__':
